0364277a4f60d15cfca056d6514d96070dfb5fc7
askerbekov <askerbekov.mederbek@gmail.com>
2023-07-16 22:06:19 +0600

    js lesson 5 homework

---
+++ script.js
@@ -0,0 +1,73 @@
+
+ // function chanchedLorem(txt) {
+ //     return txt.map((el) => el.length)
+ // }
+ // console.log( chanchedLorem(  ['lorem', 'ipsum', 'dolor', 'sit'] ))
+
+ // let txt = ['lorem', 'ipsum', 'dolor']
+ // console.log(txt.map((el, index) => index + 1))
+
+ // let txt = ['lorem', 'ipsum', 'dolor']
+ // console.log(txt.map((el, index) =>index  % 2 ===0 ? el[0].toUpperCase() + el.slice(1): el ))
+
+
+ // let txt = ['lorem', 'ipsum', 'dolor']
+ // console.log(txt.map((el , index) => el.toUpperCase()))
+
+// let txt = ['lorem', 'ipsum', 'dolor', 'sit', 'amet']
+// console.log(txt.join('-'))
+
+ // let txt = ['lorem', 'ipsum', 'dolor', 'sit', 'amet']
+ // console.log(txt.join("/"))
+
+
+ // let txt = ['lorem', 'ipsum', 'dolor', 'sit', 'amet']
+ //  console.log(txt.join(" "))
+
+ // =============   no result     =============
+ // let txt = ['lorem', 'ipsum', 'dolor', 'sit', 'amet']
+ //
+ // console.log(txt.map(( el, index) =>  index % 2 === 0  ? el : index) )
+
+
+ // let txt = ['lorem', 'ipsum', 'dolor', 'sit', 'amet']
+ // let word =  ['consectetur',' adipiscing ','elit']
+ // console.log( txt.concat(word))
+
+
+// let number = [123,33,444,'22',55,66,77,88,99
+
+ // function findAverage(array) {
+ //    let result = 0
+ //     return array.map((el , index) => ())
+ // }
+ // console.log(findAverage([1,1,1]))
+
+// let arrayResult = [123,33,444,'22',55,66,77,88,99]
+// console.log(arrayResult.filter(el => el !== '22') )
+
+ // let arrayResult = [123,33,444,'22',55,66,77,88,99]
+ // let newArrayResult = arrayResult.filter(el => el !== '22')
+ // let twoTwo = arrayResult.filter(el => el === '22')
+ // console.log(newArrayResult.concat(twoTwo))
+
+ // let array =  ['null', undefined, 0, false, true, '']
+ // console.log(array.filter(el => el >= 0))
+
+
+ /*  ===============  undefined вывести не смог ================
+ let array =  ['null', undefined, 0, false, true, '']
+ let newArray = array.filter(el => el >= 0)
+ let secondNewArray = array.filter((el, index) => el === 'null'  )
+
+ console.log(newArray.concat(secondNewArray))
+ */
+
+ // let array =  [1123, 'qwe']
+ // console.log(array.concat(array))
+
+ // let array =   [null, undefined]
+ // console.log(array.concat(array))
+
+
+ // let str =  'lorem ipsum dolor sit amet'
